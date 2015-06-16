67abc15b9f3e9eae6e2b3527c73410e896da0ae8
Jeremie Huchet <jeremie.huchet@orange.com>
2015-06-16 21:12:53 +0200

    rename image

--- proxy.sh
+++ proxy.sh
@@ -7,23 +7,23 @@ set -e
 case "$1" in
 drop)
   $0 stop
-  docker rm proxy-oab
+  docker rm transparent-auth-proxy
   ;;
 create)
-  docker run --name proxy-oab -d -e username=USERNAME -e password=PASSWORD -e proxy=PROXY_HOST:PROXY_PORT -p 33128:3128 proxy-oab:latest
+  docker run --name transparent-auth-proxy -d -e username=USERNAME -e password=PASSWORD -e proxy=PROXU_HOST:PROXY_PORT -p 33128:3128 transparent-auth-proxy:latest
   sudo iptables -t nat -A $FORWARD_TO_PROXY
   ;;
 start)
-  docker start proxy-oab
+  docker start transparent-auth-proxy
   sudo iptables -t nat -A $FORWARD_TO_PROXY
   ;;
 stop)
   sudo iptables -t nat -D $FORWARD_TO_PROXY
-  docker stop proxy-oab || docker kill proxy-oab
+  docker stop transparent-auth-proxy || docker kill transparent-auth-proxy
   ;;
 ps)
   docker ps | head -1
-  docker ps | grep proxy-oab
+  docker ps | grep transparent-auth-proxy
   ;;
 *)
   echo "Usage: $0 [create|start|stop]"
