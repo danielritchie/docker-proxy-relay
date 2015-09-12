c039bad659e4f4eb3aa50dadf6ed2a2706c0727e
danielritchie <ritchieface@gmail.com>
2015-09-12 16:22:44 -0600

    Enhance handling of password in cntlm container

--- etc/startup.sh
+++ etc/startup.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-sed -i "s/<username>/$username/g" /etc/cntlm.conf
-sed -i "s/<proxy>/$proxy/g" /etc/cntlm.conf
-sed -i "s/<domain>/$domain/g" /etc/cntlm.conf
+#Vars managed by start wrapper:
+# proxy
+# username
+# domain
 
-cntlm -c /etc/cntlm.conf -p $password
+#config file path (default is set currently)
+CNTML_CFG_FILE="/etc/cntml.conf"
+
+#Substitue provided values in config file
+sed -i "s/<username>/$username/g" $CNTML_CFG_FILE
+sed -i "s/<proxy>/$proxy/g" $CNTML_CFG_FILE
+sed -i "s/<domain>/$domain/g" $CNTML_CFG_FILE
+
+#Update config file with hashed password value
+echo "Auth         NTLM
+" >> $CNTML_CFG_FILE
+echo `cntlm -v -c /etc/cntlm.conf -M "http://www.google.com" <<<$password | grep PassNT` >> $CNTML_CFG_FILE
+echo `cntlm -v -c /etc/cntlm.conf -M "http://www.google.com" <<<$password | grep PassLM` >> $CNTML_CFG_FILE
+
+cntlm -c $CNTML_CFG_FILE
 redsocks -c /etc/redsocks.conf
