50166e1f7af6ec39ed14ff1ec7209a42264a6f42
danielritchie <ritchieface@gmail.com>
2015-09-12 10:34:57 -0600

    Add support for controlling domain with a variable.  Note that this may break use of existing ad-subs domain (to mitigate, set domain in cntlm.conf or enter it at startup)

--- docker_proxy.sh
+++ docker_proxy.sh
@@ -6,6 +6,7 @@ set -e
 proxy_host=localhost
 proxy_port=3128
 proxy_user=
+proxy_domain=
 
 # load from configuration
 test -f config && . config
@@ -16,12 +17,14 @@ case "$1" in
 start)
   read -p "Proxy host: ($proxy_host) " input && proxy_host="${input:-$proxy_host}"
   read -p "Proxy port: ($proxy_port) " input && proxy_port="${input:-$proxy_port}"
+  read -p "Proxy domain: ($proxy_domain) " input && proxy_domain="${input:-$proxy_domain}"
   read -p "$proxy_host:$proxy_port username: ($proxy_user) " input && proxy_user="${input:-$proxy_user}"
   read -s -p "$proxy_user@$proxy_host:$proxy_port password: " proxy_pass && echo
 
   docker run --name docker-proxy -d -p 33128:3128 \
       -e username=$proxy_user \
       -e password=$proxy_pass \
+      -e domain=$proxy_domain
       -e proxy=$proxy_host:$proxy_port \
       kops/docker-proxy-relay:2.0
   sudo iptables -t nat -A $FORWARD_TO_PROXY
