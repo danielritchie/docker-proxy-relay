c3820a0790077c3f8be5b213b209ef0925a8e881
danielritchie <ritchieface@gmail.com>
2015-09-14 12:04:50 -0600

    Various minor updates

--- etc/startup.sh
+++ etc/startup.sh
@@ -7,21 +7,21 @@
 # password
 
 #config file path (default is set currently)
-CNTML_CFG_FILE="/etc/cntml.conf"
+CNTML_CFG_FILE="/etc/cntlm.conf"
 
 #Substitue provided values in config file
-sed -i "s/<username>/$username/g" $CNTML_CFG_FILE
-sed -i "s/<proxy>/$proxy/g" $CNTML_CFG_FILE
-sed -i "s/<domain>/$domain/g" $CNTML_CFG_FILE
+sed -i "s/<username>/$username/g" $CNTLM_CFG_FILE
+sed -i "s/<proxy>/$proxy/g" $CNTLM_CFG_FILE
+sed -i "s/<domain>/$domain/g" $CNTLM_CFG_FILE
 
 #Update config file with hashed password value
 echo "
 Auth         NTLM
-" >> $CNTML_CFG_FILE
-echo `cntlm -v -c /etc/cntlm.conf -M "http://www.google.com" <<<$password | grep PassNT` >> $CNTML_CFG_FILE
-echo `cntlm -v -c /etc/cntlm.conf -M "http://www.google.com" <<<$password | grep PassLM` >> $CNTML_CFG_FILE
+" >> $CNTLM_CFG_FILE
+echo `cntlm -v -c $CNTLM_CFG_FILE -M "http://www.google.com" <<<$password | grep PassNT` >> $CNTLM_CFG_FILE
+echo `cntlm -v -c $CNTLM_CFG_FILE -M "http://www.google.com" <<<$password | grep PassLM` >> $CNTLM_CFG_FILE
 echo "
-" >> $CNTML_CFG_FILE
+" >> $CNTLM_CFG_FILE
 
-cntlm -c $CNTML_CFG_FILE
+cntlm -c $CNTLM_CFG_FILE
 redsocks -c /etc/redsocks.conf
